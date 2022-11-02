309bfc27003da9b76ea701598f0fb148af935ad6
Rachel Kauffman <109226895+RachelKauffman@users.noreply.github.com>
2022-11-02 08:54:35 -0400

    Added methodNotAllowed to routes

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -2,7 +2,7 @@ const router = require("express").Router({mergeParams:true});
 const methodNotAllowed = require("../errors/methodNotAllowed");
 const controller = require("./orders.controller")
 const { application } = require("express");
-const dishesRouter = require("../dishes/dishes.router")
+
 
 
 // TODO: Implement the /orders routes needed to make the tests pass
@@ -11,10 +11,12 @@ const dishesRouter = require("../dishes/dishes.router")
 router.route("/")
 .get(controller.list)
 .post(controller.create)
+.all(methodNotAllowed)
 
 router.route("/:orderId")
 .get(controller.read)
 .put(controller.update)
 .delete(controller.delete)
+.all(methodNotAllowed)
 
 module.exports = router;
